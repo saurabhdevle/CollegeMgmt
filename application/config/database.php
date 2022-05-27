366ca1fdd6e64851114dc05877ad16a5cdcd3b73
Saurabh <saurabhdevle@gmail.com>
2022-05-27 16:16:45 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'mymigrationdb.cteiqyukydpe.ap-south-1.rds.amazonaws.com',
+	'username' => 'admin',
+	'password' => 'admin',
+	'database' => 'college_mgmt',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
